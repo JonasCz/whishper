18a1d60b3752fb9ac7a94790b1aa8f3694e4afcf
Jonas <anandajonas9@gmail.com>
2024-01-23 13:51:21 +0100

    try again

--- frontend/src/lib/components/ModalTranscriptionForm.svelte
+++ frontend/src/lib/components/ModalTranscriptionForm.svelte
@@ -47,7 +47,8 @@
 	];
 
 	let models = [
-		'large-v3'
+		'large-v3',
+		'/volume/app/whisper-models/Whisper-v3-large-vietnamese-14WER-ct2'
 	];
 	// Sort the languages
 	//languages.sort((a, b) => {
